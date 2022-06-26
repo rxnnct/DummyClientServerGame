0d404bc1d37e16c478b6b3b9ec02b6ebe26af71e
Rixen <rixennoct@rambler.ru>
2022-06-26 21:33:50 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Dummy client-server game
+
+## Monorepo:
+* Website
+* Server (todo: separate to game server and login server)
+* Client (first there will be a stub. Todo: unity3d client or something else)
+
+## Implementation details:
+* Kotlin
+* Spring Framework
+* Apache Maven
---
+++ website/src/main/kotlin/ru/rxnnct/dummyclientservergame/website/WebsiteApplication.kt
@@ -0,0 +1,11 @@
+package ru.rxnnct.dummyclientservergame.website
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebsiteApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebsiteApplication>(*args)
+}
